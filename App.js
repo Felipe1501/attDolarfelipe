57abea8436b61f1eeed0dc6207e7615dc276f098
Felipe1501 <felipebsantos1501@gmail.com>
2022-06-09 23:00:49 -0300

    criando função para verificar a cotação do Dólar

--- App.js
+++ App.js
@@ -1,11 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
+
 
 export default function App() {
+
+  
+
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.texto}>HELLO HELLO!</Text>
+      <View style={styles.texto}>
+          <Text style={styles.texto}>COTAÇÃO MOEDAS:</Text>
+
+          <TouchableOpacity
+          style={styles.bloco}>
+              <Text style={styles.txtBloco}>BUSCAR</Text>
+          </TouchableOpacity>
+
+
+
+
+      </View>
     </View>
   );
 }
@@ -13,8 +30,36 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#E6F5FF',
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  texto:{
+      fontSize: 25,
+
+  },
+
+  input: {
+    width: 200,
+    borderBottomWidth: 2,
+    fontSize: 20,
+    borderRadius: 10,
+    marginTop: '3%',
+    textAlign: 'center',
+    backgroundColor: '#E6F5FF'
+  },
+
+  txtBloco: {
+    marginTop: 10,
+      backgroundColor: '#1FC9CC',
+      borderWidth: 2,
+      borderColor: '#E6F5FF',
+      borderRadius: 10,
+      width: 200,
+      textAlign: 'center',
+      height: 40, 
+      padding: 5, 
+      fontSize: 20,
+    },
 });
